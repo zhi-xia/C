18636e505a952ff37840517708442749b737efa5
zhi-xia <94606851+zhi-xia@users.noreply.github.com>
2022-06-05 22:52:57 +0800

    changes

--- 自定义数据类型/test.c
+++ 自定义数据类型/test.c
@@ -1,22 +1,210 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-#include <stddef.h>
-struct S
-{
-	char c;
-	int i;
-	double d;
-};
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	enum Sex s = MALE;
+//	printf("%d\n", sizeof(s)); //4
+//
+//	return 0;
+//}
 
-int main()
-{
-	//offsetof - 宏  可以求出偏移量
-	printf("%d\n", offsetof(struct S, c));
-	printf("%d\n", offsetof(struct S, i));
-	printf("%d\n", offsetof(struct S, d));
-	return 0;
-}
+
+
+//int check_sys()
+//{
+//	int a = 1;
+//	//返回1表示小端
+//	//返回0表示大端
+//	return *(char*)&a;
+//}
+
+//int check_sys()
+//{
+//	union Un
+//	{
+//		char c;
+//		int i;
+//	}u;
+//	u.i = 1;
+//	//返回1表示小端
+//	//返回0表示大端
+//	return u.c;
+//}
+//
+//int main()
+//{
+//	//int a = 0x11223344;
+//	//低地址---------------->高地址
+//	//...[][][][11][22][33][44][][][]... 大端字节序存储模式
+//	//...[][][][44][33][22][11][][][]... 小端字节序存储模式
+//	int ret = check_sys();
+//	if (1 == ret)
+//		printf("小端\n");
+//	else
+//		printf("大端\n");
+//
+//	return 0;
+//}
+
+
+
+////联合 - 联合体 - 共同体
+////联合体是公用一块内存空间的
+////内存大小至少是最大成员的大小
+////当最大成员大小不是最大对齐数的整数倍时，就要对齐到最大对齐数的整数倍
+//union Un
+//{
+//	char c;
+//	int i;
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));//4个字节
+//	//都是相同的地址
+//	printf("%p\n", &u);
+//	printf("%p\n", &(u.c));
+//	printf("%p\n", &(u.i));
+//
+//	return 0;
+//}
+
+
+////枚举类型
+//enum Sex
+//{
+//	//枚举的可能取值 - 常量
+//	//可以初始化，但是后面不能修改
+//	MALE = 2,
+//	FEMALE = 4,
+//	SECRET = 8
+//};
+//
+//enum Color
+//{
+//	RED,
+//	GREEN,
+//	BLUE
+//};
+//
+//int main()
+//{
+//	enum Sex s = MALE;
+//	s = FEMALE;
+//	enum Color c = BLUE;
+//	printf("%d %d %d\n", RED, GREEN, BLUE); // 0 1 2
+//	printf("%d %d %d\n", MALE, FEMALE, SECRET); // 2 4 8
+//
+//	return 0;
+//}
+
+
+
+//struct S
+//{
+//	char a : 3;
+//	char b : 4;
+//	char c : 5;
+//	char d : 4;
+//};
+//
+//int main()
+//{
+//	struct S s = { 0 };
+//	s.a = 10;
+//	s.b = 20;
+//	s.c = 3;
+//	s.d = 4;
+//	//在内存中是 22 03 04
+//	return 0;
+//}
+
+
+
+////位段 - 二进制位
+////位段可以很好的节省空间，但是跨平台有问题存在
+//struct S
+//{
+//	int a : 2;
+//	int b : 5;
+//	int c : 10;
+//	int d : 30;
+//	//int e : 33; err 不能超过类型大小32
+//};
+////47bit - 占6个字节
+//
+//int main()
+//{
+//	struct S s;
+//	struct S2 s2;
+//	printf("%d\n", sizeof(s)); //8
+//	printf("%d\n", sizeof(s2)); //3
+//	return 0;
+//}
+
+
+
+//struct S
+//{
+//	int a;
+//	char c;
+//	double d;
+//};
+//
+//void Init(struct S* ps)
+//{
+//	ps->a = 100;
+//	ps->c = 'w';
+//	ps->d = 3.14;
+//}
+////传值 - 是结构体类型的拷贝，占空间比较大
+//void Print1(struct S tmp)
+//{
+//	printf("%d %c %lf\n", tmp.a, tmp.c, tmp.d);
+//}
+////传址 - 传的是地址，空间开销比较小
+//void Print2(const struct S* ps)
+//{
+//	printf("%d %c %lf\n", ps->a, ps->c, ps->d);
+//}
+//
+//int main()
+//{
+//	struct S s = { 0 };
+//	Init(&s);
+//	Print1(s);
+//	Print2(&s);
+//
+//	return 0;
+//}
+
+
+
+//#include <stddef.h>
+//struct S
+//{
+//	char c;
+//	int i;
+//	double d;
+//};
+//
+//int main()
+//{
+//	//offsetof - 宏  可以求出偏移量
+//	printf("%d\n", offsetof(struct S, c));
+//	printf("%d\n", offsetof(struct S, i));
+//	printf("%d\n", offsetof(struct S, d));
+//	return 0;
+//}
 
 
 
