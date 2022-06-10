38458b5e6254fe91ede3db222316071a2fc8332c
zhi-xia <94606851+zhi-xia@users.noreply.github.com>
2022-06-10 14:42:43 +0800

    changes

--- 通讯录/contact.c
+++ 通讯录/contact.c
@@ -54,3 +54,135 @@ void ShowContact(const struct Contact* ps)
 		}
 	}
 }
+
+static int FindByName(const struct Contact* ps, char name[MAX_NAME])
+{
+	int i = 0;
+	for (i = 0; i < ps->size; i++)
+	{
+		if (0 == strcmp(ps->data[i].name, name))
+		{
+			return i;
+		}
+	}
+	//找不到
+	return -1;
+}
+
+void DelContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要删除人的名字:>");
+	scanf("%s", name);
+	//1.查找要删除的人在什么位置
+	int pos = FindByName(ps, name);
+	//2.删除
+	if (pos == -1)
+	{
+		printf("要删除的人不存在\n");
+	}
+	else
+	{
+		//删除数据
+		int j = 0;
+		for (j = pos; j < ps->size - 1; j++)
+		{
+			ps->data[j] = ps->data[j + 1];
+		}
+		ps->size--;
+		printf("删除成功\n");
+	}
+
+}
+
+
+void SearchContact(const struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要查找人的名字:>");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("要查找的人不存在\n");
+	}
+	else
+	{
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");
+		printf("%-20s\t%-4d\t%-5s\t%-12s\t%-20s\n",
+			ps->data[pos].name,
+			ps->data[pos].age,
+			ps->data[pos].sex,
+			ps->data[pos].tele,
+			ps->data[pos].addr);
+	}
+}
+
+
+void ModifyContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要修改人的名字:>");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("要修改的人不存在\n");
+	}
+	else
+	{
+		printf("请输入名字:>");
+		scanf("%s", ps->data[pos].name);
+		printf("请输入年龄:>");
+		scanf("%d", &(ps->data[pos].age));
+		printf("请输入性别:>");
+		scanf("%s", ps->data[pos].sex);
+		printf("请输入电话:>");
+		scanf("%s", ps->data[pos].tele);
+		printf("请输入地址:>");
+		scanf("%s", ps->data[pos].addr);
+
+		printf("修改成功\n");
+	}
+}
+
+//比较两个名字的函数
+int CmpByName(const void* e1, const void* e2)
+{
+	return strcmp(((struct PeoInfo*)e1)->name, ((struct PeoInfo*)e2)->name);
+}
+//交换函数
+void Swap(char* buf1, char* buf2, int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		buf1++;
+		buf2++;
+	}
+}
+//冒泡排序
+void BubbleSort(void* base, int sz, int width, int (*cmp)(void* e1, void* e2))
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz - 1; j++)
+		{
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0) //比较
+			{
+				//交换
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+			}
+		}
+	}
+}
+
+void SortContact(struct Contact* ps)
+{
+	BubbleSort(ps->data, ps->size, sizeof(ps->data), CmpByName);
+}
--- 通讯录/contact.h
+++ 通讯录/contact.h
@@ -10,6 +10,17 @@
 #include <stdio.h>
 #include <string.h>
 
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT
+};
+
 struct PeoInfo
 {
 	char name[MAX_NAME]; //名字
@@ -27,6 +38,23 @@ struct Contact
 
 
 //声明函数
+//初始化通讯录的函数
 void InitContact(struct Contact* ps);
+
+//增加一个信息到通讯录
 void AddContact(struct Contact* ps);
+
+//打印通讯录中的信息
 void ShowContact(const struct Contact* ps);
+
+//删除指定的联系人
+void DelContact(struct Contact* ps);
+
+//查找指定人的信息
+void SearchContact(const struct Contact* ps);
+
+//修改指定联系人
+void ModifyContact(struct Contact* ps);
+
+//排序
+void SortContact(struct Contact* ps);
